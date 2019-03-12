d721a5e164061f01b1e4cb86df028de54979fe02
Nick Lowery <ClockVapor@users.noreply.github.com>
2019-03-12 10:04:30 -0600

    Update group and exclude dependencies with kotlin runtime

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     kotlin("jvm") version "1.3.21"
 }
 
-group = "clockvapor"
+group = "clockvapor.telegram"
 version = "0.0.0"
 
 repositories {
@@ -15,7 +15,10 @@ repositories {
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
-    compile("io.github.seik", "kotlin-telegram-bot", "0.3.6")
+    compile("io.github.seik", "kotlin-telegram-bot", "0.3.6") {
+        exclude("io.github.seik.kotlin-telegram-bot", "echo")
+        exclude("io.github.seik.kotlin-telegram-bot", "dispatcher")
+    }
 }
 
 tasks.withType<KotlinCompile> {
